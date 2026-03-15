2b01604716c3785477686aad12ece9ea6489d0b1
Kroah <44671782+Kroah@users.noreply.github.com>
2026-03-15 21:53:40 +0100

    Fix front image handling in cover art provider

--- picard/coverart/providers/caa.py
+++ picard/coverart/providers/caa.py
@@ -305,6 +305,7 @@ class CoverArtProviderCaa(CoverArtProvider):
                     if not image['types']:
                         # handle unknown as front
                         image['types'] = ['front']
+                        image['front'] = True
                     else:
                         image['types'] = [t.lower() for t in image['types']]
 
@@ -329,7 +330,7 @@ class CoverArtProviderCaa(CoverArtProvider):
                         coverartimage = self.coverartimage_class(
                             url,
                             types=image['types'],
-                            is_front=image['front'] or (image['types']=='front'),
+                            is_front=image['front'],
                             comment=image['comment'],
                         )
                         if urls and is_pdf:
@@ -338,7 +339,7 @@ class CoverArtProviderCaa(CoverArtProvider):
                             thumbnail = self.coverartimage_thumbnail_class(
                                 url=urls[0],
                                 types=image['types'],
-                                is_front=image['front'] or (image['types']=='front'),
+                                is_front=image['front'],
                                 comment=image['comment'],
                             )
                             self.queue_put(thumbnail)
@@ -348,7 +349,7 @@ class CoverArtProviderCaa(CoverArtProvider):
                         self.queue_put(coverartimage)
                         if config.setting['save_only_one_front_image'] and \
                                 config.setting['save_images_to_files'] and \
-                                (image['front'] or (image['types']=='front')):
+                                image['front']:
                             break
             except (AttributeError, KeyError, TypeError) as e:
                 self.error("CAA JSON error: %s" % e)
